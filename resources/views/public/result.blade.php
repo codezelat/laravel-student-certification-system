a591645be5b3d8e19548d7b5b27a70c63943238f
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2026-02-07 04:40:48 +0530

    Block search engine indexing
    - robots.txt: Disallow all crawlers with Disallow: /
    - Added noindex, nofollow meta tags to all 9 templates
    - Affected: public pages, admin, auth, 404, welcome

--- resources/views/public/result.blade.php
+++ resources/views/public/result.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" class="h-full bg-gray-50">
 <head>
     <meta charset="UTF-8">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificate - {{ $form->title }}</title>
     
